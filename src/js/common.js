5c9f7979d7ecce850221ccf6084a0a05131f7089
yushinbox@gmail.com <yushinbox@gmail.com>
2020-09-10 17:43:48 +0300

    +

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,7 @@ import $ from "jquery";
 
 // ==========ibg=========
 function ibg() {
-  $.each($(".ibg"), function (index, val) {
+  $.each($(".board24_ibg"), function (index, val) {
     if ($(this).find("img").length > 0) {
       $(this).css(
         "background-image",
--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,10 +1,8 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-
-
 
 const PATHS = {
   src: path.join(__dirname, "../src"),
@@ -12,7 +10,9 @@ const PATHS = {
   assets: "assets/",
 };
 
-const PAGES_DIR = "src/pug/pages/index.pug"
+// const PAGES_DIR = "src/pug/pages/index.pug";
+// const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+// const PAGES = fs.readdirSync(PAGES_DIR).filter((fileName) => fileName.endsWith(".pug"));
 
 module.exports = {
   externals: {
@@ -122,14 +122,20 @@ module.exports = {
       template: "src/pug/pages/index.pug",
       filename: "./index.html",
     }),
+    new HtmlWebpackPlugin({
+      // hash: false,
+      template: "src/pug/pages/parts.pug",
+      filename: "./parts.html",
+    }),
+    //  ...PAGES.map(page => new HtmlWebpackPlugin({
+    //   template: `${PAGES_DIR}/${page}`,
+    //   filename: `./${page.replace(/\.pug/,'.html')}`
+    // })),
     new CopyWebpackPlugin({
       patterns: [
         { from: "src/img", to: "assets/img" },
         { from: "src/fonts", to: "assets/fonts" },
-    
       ],
     }),
-  
-   
   ],
-};  
+};
