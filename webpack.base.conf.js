8ef89a363ac6c9a7946a99f5c38d3d94084ab481
yushinbox@gmail.com <yushinbox@gmail.com>
2020-09-18 15:28:20 +0300

    +

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -157,6 +157,11 @@ module.exports = {
       template: "src/pug/pages/inter.pug",
       filename: "./inter.html",
     }),
+    new HtmlWebpackPlugin({
+      // hash: false,
+      template: "src/pug/pages/inter-1.pug",
+      filename: "./inter-1.html",
+    }),
     //  ...PAGES.map(page => new HtmlWebpackPlugin({
     //   template: `${PAGES_DIR}/${page}`,
     //   filename: `./${page.replace(/\.pug/,'.html')}`
